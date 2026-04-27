cedb9f77e07ec27ecfb9b713677cffa25ac76c1c
vidhithakkar28 <vidhithakkar287@gmail.com>
2026-04-27 12:16:00 +0530

    Fix: Buster browser cache for CSS/JS

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
